ef53ea713a45208b5c1df637a4b156f876ee7a99
Nikita Balashov <noWorkingEmail@mail.ru>
2023-07-05 17:56:49 +0500

    удалены туду которые были сделаны

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,3 @@
-@php 
-    // TODO: Переделать все на компоненты
-@endphp
-
 @extends('templates.main')
 @section('content')
     <svg class="fixed bottom-0 fill-green-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
